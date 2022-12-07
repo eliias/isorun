f03f10f2e69cd01395cadd8761dc8f760fa1164f
Hannes Moser <box@hannesmoser.at>
2022-12-07 14:01:39 +0100

    chore: refactor into context, modules and module items

--- ext/isorun/src/js/module_item.rs
+++ ext/isorun/src/js/module_item.rs
@@ -1,60 +1,31 @@
-use crate::isorun::utils::convert_v8_to_ruby;
-use crate::js::context::Context;
 use crate::js::worker::WORKER;
 use magnus::QNIL;
-use v8::{Global, Value};
+use v8::Global;
 
-pub(crate) enum ModuleItem<'context, 'worker> {
-    Value(ModuleItemValue<'context, 'worker>),
-    Function(ModuleItemFunction<'context, 'worker>),
-    Undefined(ModuleItemValue<'context, 'worker>),
+pub(crate) enum ModuleItem {
+    Value(Value),
+    Function(Function),
 }
 
-pub(crate) struct ModuleItemValue<'context, 'worker> {
-    pub(crate) context: &'context Context<'worker>,
-    pub(crate) export_name: String,
-    pub(crate) binding: Global<Value>,
+pub(crate) struct Function {
+    pub(crate) binding: Global<v8::Value>,
 }
 
-impl<'context, 'worker> ModuleItemValue<'context, 'worker> {
-    pub(crate) fn to_ruby(&self) -> Option<magnus::Value> {
-        WORKER.with(|worker| {
-            let mut scope = self.context.scope();
-            let value =
-                v8::Local::<Value>::new(&mut scope, self.binding.clone());
-
-            let result = convert_v8_to_ruby(value, &mut scope);
-            match result {
-                Ok(value) => Some(value),
-                Err(_) => None,
-            }
-        })
-    }
-}
-
-pub(crate) struct ModuleItemFunction<'context, 'worker> {
-    pub(crate) context: &'context Context<'worker>,
-    pub(crate) export_name: String,
-    pub(crate) binding: Global<Value>,
-}
-
-impl<'context, 'worker> ModuleItemFunction<'context, 'worker> {
+impl Function {
     pub(crate) fn call(
         &self,
-        args: &[Global<Value>],
+        args: &[Global<v8::Value>],
     ) -> Result<magnus::Value, magnus::Error> {
-        Ok(magnus::Value::from(QNIL))
+        WORKER.with(|worker| worker.call(&self.binding, args))
     }
 }
 
-pub(crate) struct ModuleItemUndefined<'context, 'worker> {
-    pub(crate) context: &'context Context<'worker>,
-    pub(crate) export_name: String,
-    pub(crate) binding: Global<Value>,
+pub(crate) struct Value {
+    pub(crate) binding: Global<v8::Value>,
 }
 
-impl<'context, 'worker> ModuleItemUndefined<'context, 'worker> {
+impl Value {
     pub(crate) fn to_ruby(&self) -> Option<magnus::Value> {
-        Some(magnus::Value::from(QNIL))
+        WORKER.with(|worker| worker.to_ruby(&self.binding))
     }
 }
--- ext/isorun/src/lib.rs
+++ ext/isorun/src/lib.rs
@@ -1,5 +1,6 @@
+use crate::isorun::context::Context;
 use isorun::module::Module;
-use isorun::module_item::ModuleItem;
+use isorun::module_item::{Function, Value};
 use magnus::{define_module, function, method, Error, Module as M, Object};
 
 mod isorun;
@@ -9,43 +10,39 @@ mod js;
 fn init() -> Result<(), Error> {
     let root = define_module("Isorun").expect("cannot define module: Isorun");
 
+    let context = root
+        .define_class("Context", Default::default())
+        .expect("cannot define class: Isorun::Context");
+    context
+        .define_singleton_method("create", function!(Context::create, 0))
+        .expect("cannot define singelton method: create");
+    context
+        .define_method("load", method!(Context::load, 1))
+        .expect("cannot load module");
+
     let module = root
         .define_class("Module", Default::default())
         .expect("cannot define class: Isorun::Module");
     module
-        .define_singleton_method("new", function!(Module::new, -1))
-        .expect("cannot define singelton method: new");
-    module
-        .define_private_method("module_id", method!(Module::module_id, 0))
+        .define_private_method("id", method!(Module::id, 0))
         .expect("cannot define method: module_id");
     module
-        .define_private_method(
-            "module_import",
-            method!(Module::module_import, 1),
-        )
+        .define_method("import", method!(Module::import, 1))
         .expect("cannot define method: module_import");
 
-    let module_item = root
-        .define_class("ModuleItem", Default::default())
-        .expect("cannot define class: Isorun::ModuleItem");
-    module_item
-        .define_private_method(
-            "module_item_call",
-            method!(ModuleItem::module_item_call, -1),
-        )
-        .expect("cannot define method: module_item_call");
-    module_item
-        .define_private_method(
-            "module_item_export_name",
-            method!(ModuleItem::module_item_export_name, 0),
-        )
-        .expect("cannot define method: module_item_export_name");
-    module_item
-        .define_private_method(
-            "module_item_to_value",
-            method!(ModuleItem::module_item_to_value, 0),
-        )
-        .expect("cannot define method: module_item_to_value");
+    let function = root
+        .define_class("Function", Default::default())
+        .expect("cannot define class: Isorun::Function");
+    function
+        .define_method("call", method!(Function::call, -1))
+        .expect("cannot define method: call");
+
+    let value = root
+        .define_class("Value", Default::default())
+        .expect("cannot define class: Isorun::Value");
+    value
+        .define_method("to_ruby", method!(Value::to_ruby, 0))
+        .expect("cannot define method: to_ruby");
 
     Ok(())
 }
